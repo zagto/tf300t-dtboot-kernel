f649459c554b5fdfe07e76ca83b2d7d87ab878b2
Tobias Zagorni <tzagorni@gmx.de>
2018-12-23 16:34:56 +0100

    map buttons to up, down and enter for use in dtbootmenu

--- arch/arm/mach-tegra/board-cardhu-kbc.c
+++ arch/arm/mach-tegra/board-cardhu-kbc.c
@@ -49,9 +49,10 @@
 	}
 
 static struct gpio_keys_button cardhu_int_keys[] = {
-	[0] = GPIO_KEY(KEY_POWER, PV0, 1),
-	[1] = GPIO_KEY(KEY_VOLUMEUP, PQ2, 0),
-	[2] = GPIO_KEY(KEY_VOLUMEDOWN, PQ3, 0),
+	/* make keys trigger dtbootmenu functions */
+	[0] = GPIO_KEY(KEY_ENTER, PV0, 1),
+	[1] = GPIO_KEY(KEY_UP, PQ2, 0),
+	[2] = GPIO_KEY(KEY_DOWN, PQ3, 0),
 };
 
 static struct gpio_keys_platform_data cardhu_int_keys_pdata = {
